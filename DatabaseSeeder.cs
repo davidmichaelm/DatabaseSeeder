defe85aa678bf75c18b3bf8c1d40af182fa939e7
davidmichaelm <davemarquardt@gmail.com>
2021-01-19 15:49:01 -0600

    Add methods to generate random events

--- DatabaseSeeder.cs
+++ DatabaseSeeder.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using SeedDatabase.Model;
 
@@ -5,11 +6,14 @@ namespace SeedDatabase
 {
     public class DatabaseSeeder
     {
+        private Random _random;
         public void Run()
         {
-            List<Location> locations = GenerateLocations();
+            _random = new Random();
+            
+            var locations = GenerateLocations();
             // PopulateLocationsTable(locations);
-            // List<Event> events = GenerateRandomEvents();
+            var events = GenerateRandomEvents(locations);
             // PopulateEventsTable(events);
             // ReadEventsTable();
         }
@@ -34,6 +38,59 @@ namespace SeedDatabase
                     Name = "Kitchen Light"
                 }
             };
+        }
+        
+        private List<Event> GenerateRandomEvents(List<Location> locations)
+        {
+            var events = new List<Event>();
+            var totalNumDays = 180; // generate events for 6 months
+
+            for (var day = 0; day < totalNumDays; day++)
+            {
+                var numEvents = _random.Next(0, 6);
+
+                for (var j = 0; j < numEvents; j++)
+                {
+                    var date = DateTime.Today.AddDays(day);
+                    var newEvent = GenerateRandomEvent(events.Count, locations, date);
+                    events.Add(newEvent);
+                }
+            }
+
+            return events;
+        }
+
+        private Event GenerateRandomEvent(int eventId, List<Location> locations, DateTime date)
+        {
+            var location = GetRandomLocation(locations);
+            
+            return new Event
+            {
+                EventId = eventId,
+                TimeStamp = GetRandomTimeStamp(date),
+                Flagged = GetRandomFlag(),
+                LocationId = location.LocationId,
+                Location = location
+            };
+        }
+
+        private Location GetRandomLocation(List<Location> locations)
+        {
+            var index = _random.Next(locations.Count);
+            return locations[index];
+        }
+
+        private bool GetRandomFlag()
+        {
+            return _random.NextDouble() >= 0.5;
+        }
+
+        private DateTime GetRandomTimeStamp(DateTime date)
+        {
+            var hour = _random.Next(0, 24);
+            var minutes = _random.Next(0, 60);
+            var seconds = _random.Next(0, 60);
+            return new DateTime(date.Year, date.Month, date.Day, hour, minutes, seconds);
         }
     }
 }
