2125afe82d481893ee8c433df366884d493c2caf
davidmichaelm <davemarquardt@gmail.com>
2021-01-19 13:50:15 -0600

    Add GenerateLocations method

--- DatabaseSeeder.cs
+++ DatabaseSeeder.cs
@@ -1,14 +1,39 @@
+using System.Collections.Generic;
+using SeedDatabase.Model;
+
 namespace SeedDatabase
 {
     public class DatabaseSeeder
     {
         public void Run()
         {
-            // List<Location> locations = GenerateLocations();
+            List<Location> locations = GenerateLocations();
             // PopulateLocationsTable(locations);
             // List<Event> events = GenerateRandomEvents();
             // PopulateEventsTable(events);
             // ReadEventsTable();
+        }
+        
+        private List<Location> GenerateLocations()
+        {
+            return new List<Location>
+            {
+                new Location
+                {
+                    LocationId = 0, 
+                    Name = "Garage"
+                },
+                new Location
+                {
+                    LocationId = 1, 
+                    Name = "Thermostat"
+                },
+                new Location
+                {
+                    LocationId = 2, 
+                    Name = "Kitchen Light"
+                }
+            };
         }
     }
 }
